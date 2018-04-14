a90aaff8ee9ae907c7cf3bb2b45c72272216ed89
ahegde5@uic.edu <ahegde5@uic.edu>
2018-04-14 12:42:29 -0500

    fixed issue

--- graph.py
+++ graph.py
@@ -15,14 +15,14 @@ Used to create graphs for entities to better analyze changes
 """
 
 # create a graph node for an entity and populate its child nodes
-def create_graph_node(G, my_node):
+def create_graph_node(G, db, my_node):
     for ent in db.ents():
         if ent.parent() !=None and ent.parent().longname() == my_node.longname() :
             #print("kind",ent.kind())
             G.add_node(ent.longname(), entity=ent)
             G.add_edge(my_node.longname(),ent.longname(), relationship=ent.kind())
             #print("ent",ent.longname(),"kind",G[my_node.longname()][ent.longname()]["relationship"])
-            create_graph_node(G, ent)
+            create_graph_node(G,db, ent)
     
     
 
@@ -41,7 +41,7 @@ def create_graph():
 # create a class node
 def create_class_node(G,db,class_ent):
     G.add_node(class_ent.longname(),entity=class_ent)
-    create_graph_node(G, class_ent)
+    create_graph_node(G,db, class_ent)
 
 # compare identical method nodes in two graphs
 def compare_method_nodes(G1, G2, common_methods):
@@ -61,7 +61,8 @@ def compare_method_nodes(G1, G2, common_methods):
 def compare_class_methods(G1, G2, class_name1, class_name2):    
     methods_1 = [method_node for method_node in G1.neighbors(class_name1) if "Method" in G1[class_name1][method_node]['relationship'].name()]
     methods_2 = [method_node for method_node in G2.neighbors(class_name2) if "Method" in G2[class_name2][method_node]['relationship'].name()]
-    
+    print("Methods1", methods_1)
+    print("Methods2", methods_2)
     only_methods_1 = [x for x in methods_1 if x not in methods_2]
     print("Methods only in 1", only_methods_1)
     only_methods_2 = [x for x in methods_2 if x not in methods_1]
@@ -72,8 +73,8 @@ def compare_class_methods(G1, G2, class_name1, class_name2):
 
 
 
-db = understand.open("C:\\Understand\\v6.udb")
-db2 = understand.open("C:\\Understand\\v3.udb")
+"""db = understand.open("C:\\Understand\\v10.udb")
+db2 = understand.open("C:\\Understand\\v11.udb")
 
 
 files1 = us.get_filenames(db, '.java', '')
@@ -83,7 +84,8 @@ filenames = list(set.intersection(files1,files2))
 for filename in filenames:
     file1 = db.lookup(filename,"file")[0]
     file2 = db2.lookup(filename,"file")[0]
-    #print((file1.name()))
+    print("---------------------------------------------------------------------------")
+    print((file1.name()))
     #print((file2.name()))
     class10 = [sel_class for sel_class in db.lookup(filename.split(".")[0],"class") if sel_class.parent() == file1][0]
     class11 = [sel_class for sel_class in db2.lookup(filename.split(".")[0],"class") if sel_class.parent() == file2][0]
@@ -103,4 +105,4 @@ for filename in filenames:
 
 
 db.close()
-db2.close()
+db2.close()"""
