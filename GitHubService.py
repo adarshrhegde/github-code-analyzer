d2bf9f534e94f5035b3974f67451d3d3e59ad43e
skarim25 <skarim25@uic.edu>
2018-04-14 19:24:36 -0500

    resolving merge issues

--- GitHubService.py
+++ GitHubService.py
@@ -1,13 +1,12 @@
 
 # coding: utf-8
 
-# In[20]:
+# In[100]:
 
 
 import requests
 import subprocess
 import logging
-import os
 from UnderstandService import create_udb,execute
 from git import Repo
 from github import Github
@@ -15,10 +14,9 @@ import understand
 import  configparser 
 config =  configparser.RawConfigParser()
 config.read('GitHubVariables.properties') 
-number_of_projects=config.get('Project','number_of_projects')
+global number_of_projects
 number_of_commits=config.get('Project','number_of_commits')
 path=config.get('Project','project_path')
-print(number_of_commits)
 
 
 #github Authentication using token
@@ -26,8 +24,10 @@ headers={'Authorization':'token 5c4d81ec05cb82053d9fd0c0519120fe3eed17be',
         'User-Agent':'https://api.github.com/meta',
         'Content-Type':'application/json'}
 
-global udb_path
-global project_root
+global udb_path1
+global udb_path2
+global project_root1
+global project_root2
 global repo_name
 global projectname
 
@@ -48,53 +48,59 @@ def get_repo_url(pull_req)    : # Get the repo URL to clone the repo
     if pull_req.status_code == 200:
             pull2=pull_req.json()
             repo_url=pull2['items'][0]['repository_url']
-            print(repo_url)
+            print('Repo_URL:',repo_url)
             response=requests.get(repo_url,headers=headers)
             if pull_req.status_code == 200:
                 repo_url=response.json()['html_url']
             
     return repo_url
 
-def clone_repo(repo_url,name):
-    print('Done')
+def clone_repo(repo_url,name,num):
+    
     git_url=repo_url
    
-    repo_dir=path + name+'\\'
+    repo_dir=path + name+'\\'+str(num)
+    print('Cloning at:',repo_dir)
     Repo.clone_from(git_url, repo_dir)
     print('Done cloning')
     return repo_dir
 
 #Checkout the version of the project using the SHA for that version
-def git_checkout(sha,projectname):
-        print('Checking out for',sha)
+def git_checkout(sha,projectname,num,number_of_projects):
+        
         result = []
+        flag=True
         cmd='git checkout '+ sha
-        print(cmd)
-        print(path+projectname+'\\')
-        process = subprocess.Popen(cmd,cwd=path+projectname+'\\',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE )
-
-        for line in process.stdout:
-            result.append(line)
-            errcode = process.returncode
-        for line in result:
-            print(line)
+        
+        process = subprocess.Popen(cmd,cwd=path+projectname+'\\'+str(num)+'\\',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE )
+        result = process.communicate()[1].decode().split(':')[0]
+        #print(result)
+        if(result=="FATAL" or result=="fatal"):
+            number_of_projects=number_of_projects+1
+            flag=False
+        return flag,number_of_projects
+    
+        
+            
 
 # Searches for closed issues for repositiories and gets list of pull requests.
-# Further commit list is populated for the list of pullrequest and corresponding SHA of each commit is stored in sha_list
+# Further commit listnumber_of_projects is populated for the list of pullrequest and corresponding SHA of each commit is stored in sha_list
 # Project from the repo is cloned in user defined Path from properties file
 def git_analyzer():
     repo = requests.get('https://api.github.com/search/repositories?q=language:java',headers=headers)
     #Search for repositories with language:Java
-    print(repo)
+    
+    number_of_projects=int(config.get('Project','number_of_projects'))
 
     if repo.status_code != 403:
 
         response=repo.json()
-        for i in range(int(number_of_projects)):
-            if int(len(response['items']))< int(number_of_projects):
+        for i in range(number_of_projects):
+            if int(len(response['items']))< number_of_projects:
                 break
             repo_name=response['items'][i]['full_name']
-            print('https://api.github.com/search/issues?q=type:pr+language:java+state:closed+is:merged+status:success+repo:'+repo_name)
+            #repo_name='iluwatar/java-design-patterns'
+            #print('https://api.github.com/search/issues?q=type:pr+language:java+state:closed+is:merged+status:success+repo:'+repo_name)
             pull_req = requests.get('https://api.github.com/search/issues?q=type:pr+language:java+state:closed+is:merged+status:success+repo:'+repo_name,headers=headers)
             commit_list=list()    
 
@@ -103,22 +109,24 @@ def git_analyzer():
 
             # get the repository URL
             repo_url=get_repo_url(pull_req)
-            print('Repo_url',repo_url)
 
 
             #Clone the repository
-            repo_dir=clone_repo(repo_url,projectname)
+            repo_dir1=clone_repo(repo_url,projectname,1)
+            repo_dir2=clone_repo(repo_url,projectname,2)
 
 
             #Create the UDB for the project
 
-            print(projectname)
-            udb_path=repo_dir
-            language='java'
-            project_root=repo_dir
+           
+            udb_path1=repo_dir1
+            udb_path2=repo_dir2
+            
+        
 
             #Get pull and commit list for the project
             commit_list=get_pull_req(pull_req)
+            #print('commit list',commit_list)
             sha_list=list()
             #Iterating through list of commits to get the SHAs 
             for j in range(len(commit_list)):
@@ -133,36 +141,66 @@ def git_analyzer():
 
                         else:
                             sha_list.append(commit_json.json()[(len(commit_json.json()))-k-1]['sha'])
-            process(sha_list,projectname,udb_path,project_root)
+            number_of_projects=process(sha_list,projectname,udb_path1,udb_path2,number_of_projects)
 
 # The clone repo is then checked out to the SHA version and for every pair of consecutive versions,
 # pair of udbs are created and processed usinf execute method
-def process(sha_list,projectname,udb_path,project_root):
-            git_checkout(sha_list[0],projectname) # Checking out repo to get Base version
+def process(sha_list,projectname,udb_path1,udb_path2,number_of_projects):
+            #print(sha_list)
+            #checkout to base version
+            flag,np=git_checkout(sha_list[len(sha_list)-1],projectname,1,number_of_projects)
+            flag,np=git_checkout(sha_list[len(sha_list)-1],projectname,2,number_of_projects)
+            project_root1=udb_path1
+            project_root2=udb_path2
+            if not flag:
+                return np
+            else:
+                print('Checkout successful')
 
+                
             for l in range(int(number_of_commits)):
                 if int(len(sha_list))< int(number_of_commits):
                     break
-                print(sha_list[len(sha_list)-l-1])
+                
+              #  print(sha_list[len(sha_list)-l-1])
                 language='java'
-                create_udb(udb_path+'1', language, project_root)     
-
-    #Checkout the next version using SHA
-                git_checkout(sha_list[len(sha_list)-l-1],projectname)
-        #analyze the UDB with the new code version
-                create_udb(udb_path+'2', language, project_root) 
-                print(udb_path)
-                db1 = understand.open(r''+udb_path+'1.udb')
-                db2 = understand.open(r''+udb_path+'2.udb')                
-                execute(db1,db2,projectname, project_root)
+               
+                #Checkout the next version using SHA
+                flag,np=git_checkout(sha_list[len(sha_list)-l-2],projectname,1,number_of_projects) 
+                #Checkout the next version using SHA
+                flag,np=git_checkout(sha_list[len(sha_list)-l-3],projectname,2,number_of_projects)
+                
+                #Create analyze the UDB with the new code version
+#                print('Path',r''+udb_path1+r'\1')
+                
+                create_udb(r''+udb_path1+r'\1', language, project_root1)
+                create_udb(r''+udb_path2+r'\2', language, project_root2) 
+                path1=r''+udb_path1+r'\1.udb'
+                path2=r''+udb_path2+r'\2.udb'
+#                print(path1)
+#                print(path2)
+                try:
+                    db1 = understand.open(path1)
+                    db2 = understand.open(path2) 
+                except understand.UnderstandError as e:
+                    logging.fatal('udb open failed')
+                    raise Exception
+                #Executing Understand analysis 
+                execute(db1,db2,projectname, '1')
                 print("Execute called")
                 db1.close()
                 db2.close()
-
+                return np
 
 
 git_analyzer()
-
+#Kill Switch
 
         
+
+
+# In[89]:
+
+
+
 
